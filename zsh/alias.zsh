c4671b227bed5f4df92657cf333c7e1994cfb6c6
fujianhao3 <fujianhao3@jd.com>
2020-08-08 23:11:03 +0800

    增加了 alias.zsh 的配置修改

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -13,7 +13,7 @@ alias grep='grep --color=auto -in'
 alias v2ray='export http_proxy="127.0.0.1:8001" && export https_proxy="127.0.0.1:8001" && export all_proxy="127.0.0.1:8001"'
 alias nov2ray='export http_proxy="" && export https_proxy="" && export all_proxy=""'
 
-# git config
+# git alias config
 alias glog="git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 alias git="git --no-pager"
 
