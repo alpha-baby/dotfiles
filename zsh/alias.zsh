4f1bcef374a07db02ae00c14b8a1e09ea9c405b0
fujianhao3 <fujianhao3@jd.com>
2023-09-30 10:03:51 +0800

    add clash proxy alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -37,6 +37,18 @@ function v2() {
     all_proxy=socks5://127.0.0.1:1081 http_proxy=http://127.0.0.1:8001 https_proxy=http://127.0.0.1:8001 $@
 }
 
+_clash_proxy_addrs='all_proxy="socks5://127.0.0.1:7890" http_proxy="http://127.0.0.1:7890" https_proxy="http://127.0.0.1:7890"'
+
+#alias clash='export http_proxy="127.0.0.1:7890" https_proxy="127.0.0.1:7890" all_proxy="127.0.0.1:7890"'
+alias clash="export ${_clash_proxy_addrs}"
+alias noclash='unset http_proxy https_proxy all_proxy'
+
+function cl() {
+    echo "start clash proxy\n \
+    - command: $@\n \
+    --------------------------------------------"
+    all_proxy="socks5://127.0.0.1:7890" http_proxy="http://127.0.0.1:7890" https_proxy="http://127.0.0.1:7890" $@
+}
 # git alias config
 #alias glog="git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 # alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
